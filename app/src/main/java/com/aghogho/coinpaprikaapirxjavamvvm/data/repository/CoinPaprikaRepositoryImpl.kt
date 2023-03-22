a8446c78a73c82a6f3a2aa9f6c9f26cad9a7af33
Aghogho <aghogho.akpedeye@gmail.com>
2023-03-22 23:11:52 +0200

    setup repository interface and its impl

---
+++ app/src/main/java/com/aghogho/coinpaprikaapirxjavamvvm/data/repository/CoinPaprikaRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.aghogho.coinpaprikaapirxjavamvvm.data.repository
+
+class CoinPaprikaRepositoryImpl {
+}
---
+++ app/src/main/java/com/aghogho/coinpaprikaapirxjavamvvm/domain/repository/CoinPaprikaRepository.kt
@@ -0,0 +1,4 @@
+package com.aghogho.coinpaprikaapirxjavamvvm.domain.repository
+
+interface CoinPaprikaRepository {
+}
