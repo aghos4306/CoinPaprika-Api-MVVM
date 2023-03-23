ba3378056b612cd94120885f440bd14dea3a55e3
Aghogho <aghogho.akpedeye@gmail.com>
2023-03-23 14:31:41 +0200

    implemented short animation on coin click

--- app/src/main/java/com/aghogho/coinpaprikaapirxjavamvvm/presentation/coin_paprika_list/CoinPaprikaListScreen.kt
+++ app/src/main/java/com/aghogho/coinpaprikaapirxjavamvvm/presentation/coin_paprika_list/CoinPaprikaListScreen.kt
@@ -1,5 +1,9 @@
 package com.aghogho.coinpaprikaapirxjavamvvm.presentation.coin_paprika_list
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,23 +15,58 @@ import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aghogho.coinpaprikaapirxjavamvvm.presentation.Screen
 import com.aghogho.coinpaprikaapirxjavamvvm.presentation.coin_paprika_list.component.CoinPaprikaListItem
+import kotlinx.coroutines.launch
 
 @Composable
 fun CoinPaprikaListScreen(
     navController: NavController,
-    viewModel: CoinPaprikaListViewModel = hiltViewModel()
+    viewModel: CoinPaprikaListViewModel = hiltViewModel(),
+
+    animationDuration: Int = 100,
+    scaleDown: Float = 0.9f
 ) {
+    val interactionSource = MutableInteractionSource()
+    val coroutineScope = rememberCoroutineScope()
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(1f)
+    }
+
     val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+
+                .scale(scale = scale.value)
+                .clickable(interactionSource = interactionSource, indication = null) {
+                    coroutineScope.launch {
+                        scale.animateTo(
+                            scaleDown,
+                            animationSpec = tween(animationDuration)
+                        )
+                        scale.animateTo(
+                            1f,
+                            animationSpec = tween(animationDuration)
+                        )
+                    }
+                }
+
+
+        ) {
             items(state.coins) { coin ->
                 CoinPaprikaListItem(
                     coin = coin,
